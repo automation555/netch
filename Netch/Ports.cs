1c1c3c21c2ea0d9121c909b42921820dd5d46c8a
rajnikant <59799061+automation555@users.noreply.github.com>
2022-06-16 11:42:36 +0530

    Create Ports.cs

---
+++ Netch/Ports.cs
@@ -0,0 +1,25 @@
+﻿namespace Netch.Models.Config
+{
+    public class Ports1
+    {
+        /// <summary>
+        ///     Socks 端口
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("socks")]
+        public int Socks = 2081;
+
+        /// <summary>
+        ///     Mixed 端口
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("mixed")]
+        public int Mixed = 2082;
+
+        /// <summary>
+        ///     Redir 端口
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("redir")]
+        public int Redir = 2083;
+        
+        int x=0;
+    }
+}
